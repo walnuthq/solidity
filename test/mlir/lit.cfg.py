27ab9be854763cdcc095288bd91565b6571ee183
Djordje Todorovic <djolertrk@gmail.com>
2026-02-01 16:27:14 +0100

    Fix test infra
    % cmake -DSOLIDITY_HAS_MLIR=1 .. && make check-solidity-mlir

--- test/mlir/lit.cfg.py
+++ test/mlir/lit.cfg.py
@@ -11,7 +11,7 @@ import lit.util
 config.name = 'Solidity-MLIR'
 
 # testFormat: The test format to use to interpret tests.
-config.test_format = lit.formats.ShTest(not llvm_config.use_lit_shell)
+config.test_format = lit.formats.ShTest(execute_external=True)
 
 # suffixes: A list of file extensions to treat as test files.
 config.suffixes = ['.mlir', '.sol']
@@ -22,11 +22,15 @@ config.test_source_root = os.path.dirname(__file__)
 # test_exec_root: The root path where tests should be run.
 config.test_exec_root = os.path.join(config.solidity_obj_root, 'test', 'mlir')
 
-# Tweak the PATH to include the tools dir.
-llvm_config.with_environment('PATH', config.llvm_tools_dir, append_path=True)
+# Add tools dir to PATH so lit can find FileCheck, etc.
+path = os.path.pathsep.join([config.llvm_tools_dir, config.environment.get('PATH', '')])
+config.environment['PATH'] = path
 
 # Propagate some variables from the host environment.
-llvm_config.with_system_environment(['HOME', 'INCLUDE', 'LIB', 'TMP', 'TEMP'])
+for var in ['HOME', 'INCLUDE', 'LIB', 'TMP', 'TEMP']:
+    val = os.environ.get(var)
+    if val:
+        config.environment[var] = val
 
 # Add solc to the path
 config.substitutions.append(('%solc', config.solc_executable))
@@ -42,4 +46,3 @@ config.excludes = ['Inputs', 'CMakeFiles']
 # Add features
 if config.solidity_has_mlir:
     config.available_features.add('mlir')
-EOF < /dev/null
